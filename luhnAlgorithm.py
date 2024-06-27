cc6c4f0cea9f35841b13792650506cebfb4642cf
Mi1Col <143884635+Mi1Col@users.noreply.github.com>
2024-06-27 13:54:15 +0200

    Add files via upload

--- luhnAlgorithm.py
+++ luhnAlgorithm.py
@@ -1,31 +1,32 @@
+def luhnAlgorithm(numberCard):
+    # Pide el numero de atrjeta y comprueba si mide 16
+    if len(str(numberCard)) != 16:
+        numberCard = int(input("Introduzca su numero de tarjeta: "))
 
-# Pide el numero de tarjeta y comprueba si mide 16
-numberCard = input("Introduzca su numero de tarjeta: ")
-if len(str(numberCard)) != 16:
-    numberCard = int(input("Introduzca su numero de tarjeta: "))
+    # Invierte el numero de tarjeta
+    numberCardInv = str(numberCard)[::-1]
 
-# Invierte el numero de tarjeta
-numberCardInv = str(numberCard)[::-1]
+    # Transforma los numeros en enteros y los añade a una lista
+    lista = []
+    for i in numberCardInv:
+        lista.append(int(i))
 
-# Transforma los numeros en enteros y los añade a una lista
-lista = []
-for i in numberCardInv:
-    lista.append(int(i))
+    # Multiplica los numeros en una posicion par y si el resultado tiene dos digitos, los suma
+    for j in lista:
+        if lista.index(j) % 2 == 0:
+            a = j * 2
+            if len(str(a)) != 1:
+                suma = int(str(a)[0]) + int(str(a)[1])
+                lista[lista.index(j)] = suma
+            else:
+                lista[lista.index(j)] = a
 
-# Multiplica los numeros en una posicion par y si el resultado tiene dos digitos, los suma
-for j in lista:
-    if lista.index(j) % 2 == 0:
-        a = j * 2
-        if len(str(a)) != 1:
-            suma = int(str(a)[0]) + int(str(a)[1])
-            lista[lista.index(j)] = suma
-        else:
-            lista[lista.index(j)] = a
+    # Sumar todos los digitos
+    suma2 = sum(lista)
 
-# Sumar todos los digitos
-suma2 = sum(lista)
+    # Si la suma es divisible entre 10 (acaba en 0) el numero de tarjeta es valido
 
-# Si la suma es divisible entre 10 (acaba en 0) el numero de tarjeta es valido
-
-if suma2 % 10 == 0:
-    print("El numero de tarjeta es VALIDO")
+    if suma2 % 10 == 0:
+        print("El numero de tarjeta es VALIDO")
+    else:
+        print("El numero de tarjeta NO es VALIDO")
